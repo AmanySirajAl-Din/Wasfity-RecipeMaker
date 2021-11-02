fff621255da16f2ddca7fab94936d85d493ea9c5
AsmaaHosnyAly <49831110+AsmaaHosnyAly@users.noreply.github.com>
2021-11-02 17:36:20 +0200

    before editing

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './App.css';
 import Footer  from './components/admin/footer/Footer';
 import  NavbarSection  from './components/admin/Navbar/Navbar';
 
+import { initializeApp } from "firebase/app"
+import Sidbar from './components/admin/sidbar/Sidbar'
+
+import {useEffect} from 'react'
+import{db} from './firebase'
+import { collection, getDocs,onSnapshot } from'firebase/firestore'
+
 function App() {
+  useEffect(() => {
+    onSnapshot(collection(db,'recipes'),(snapshot)=>
+    console.log(snapshot.docs.map(doc=>doc.data())));
+   
+  });
+
   return (
     <div className="App">
-      <NavbarSection/>
-      
+      {/* <NavbarSection/> */}
+      <Sidbar/>
      <Footer/>
     </div>
   );
---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+import {getFirestore} from'firebase/firestore'; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBqimLnK8l3nS4EKjjm2oE5jG0kvOQjhVA",
+  authDomain: "recipe-maker-system.firebaseapp.com",
+  projectId: "recipe-maker-system",
+  storageBucket: "recipe-maker-system.appspot.com",
+  messagingSenderId: "20370346274",
+  appId: "1:20370346274:web:1ab42a4d4d4cfe75cc957b",
+  measurementId: "G-P2SWYKJT8L"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db=getFirestore(app);
+
+
+
+
+
+

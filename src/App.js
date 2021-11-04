34bb239624e4c5de09153e71c1b4b797d42fd252
Shouib Issam <shouibelnoby22@gmail.com>
2021-11-04 14:04:37 +0200

    I made auth with firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import './App.css';
-import Login from './pages/login/login';
-// import Signup from './pages/signup/signup';
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/login/login";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/signup/signup";
+import UserRout from "./components/UserRout/UserRout";
+import { useDispatch } from "react-redux";
+import { auth } from "./Firebase/Firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { setUser } from "./redux/authAction"; //set user or rediracte
 
 function App() {
+  const dispatch = useDispatch();
+  // I made to sure if their user set hem not rediracte to login page
+  useEffect(() => {
+    onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        dispatch(setUser(authUser));
+      } else {
+        dispatch(setUser(null));
+      }
+    });
+  }, [dispatch]);
   return (
-    <div className="App">
-      <Login />
-      {/* <Signup /> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <UserRout exact path="/" component={Home} />
+          //I custom this route in component UserRout in folder component
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Signup} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,119 +1,191 @@
-import './login.css';
-import food from '../../assets/img/pic2.png';
-import { useState } from 'react';
+import "./login.css";
+import food from "../../assets/img/pic2.png";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  fbSignInInitiate,
+  googleSignInInitiate,
+  loginInitiate,
+} from "../../redux/authAction"; // import auth
 
 function Login() {
+  const [login, setlogin] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [login, setlogin] = useState({
-        email: '',
-        password: ''
-    })
+  const [errors, setErrors] = useState({
+    email: null,
+    password: null,
+  });
+  // Social Login{
+  const GoogleSignin = () => {
+    dispatch(googleSignInInitiate());
+  };
+  const FBSignin = () => {
+    dispatch(fbSignInInitiate());
+  };
+  //}
 
-    const [errors, setErrors] = useState({
-        email: null,
-        password: null
-    })
+  const { currentUser } = useSelector((state) => state.user); //get data from redux
 
-    const submitForm = (e)=>{
-        e.preventDefault();
+  const history = useHistory();
+  // I made it to move user to home if he sign in
+  useEffect(() => {
+    if (currentUser) {
+      history.push("/");
     }
+  }, [currentUser, history]);
 
-    const eRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const emailValidation = (e)=>{
-        if(e.target.name === 'email'){
-            setlogin({
-                ...login,
-                email: e.target.value
-            })
-        }
-        setErrors({
-            ...errors,
-            email: e.target.value.length === 0 
-                    ? 'field required'
-                    : !eRegex.test(e.target.value)
-                    ? 'email not exist'
-                    : null
-        })
+  const dispatch = useDispatch();
+
+  const submitForm = (e) => {
+    //validate auth
+    e.preventDefault();
+    if (!login.email || !login.password) {
+      return;
     }
+    dispatch(loginInitiate(login.email, login.password));
+    setlogin({ email: "", password: "" });
+  };
 
-    const pRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    const passwordValidation = (e)=>{
-        if(e.target.name === 'password'){
-            setlogin({
-                ...login,
-                password: e.target.value
-            })
-        }
-        setErrors({
-            ...errors,
-            password: e.target.value.length === 0 
-                    ? 'field required'
-                    : !pRegex.test(e.target.value)
-                    ? 'wrong password'
-                    : null
-        })
+  const eRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailValidation = (e) => {
+    // data for authentication{
+    let { name, value } = e.target;
+    setlogin({ ...login, [name]: value });
+    //}
+    if (e.target.name === "email") {
+      setlogin({
+        ...login,
+        email: e.target.value,
+      });
     }
+    setErrors({
+      ...errors,
+      email:
+        e.target.value.length === 0
+          ? "field required"
+          : !eRegex.test(e.target.value)
+          ? "email not exist"
+          : null,
+    });
+  };
 
-    let url = '#';
+  const pRegex = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  const passwordValidation = (e) => {
+    // data for authentication{
+    let { name, value } = e.target;
+    setlogin({ ...login, [name]: value });
+    //}
+    if (e.target.name === "password") {
+      setlogin({
+        ...login,
+        password: e.target.value,
+      });
+    }
+    setErrors({
+      ...errors,
+      password:
+        e.target.value.length === 0
+          ? "field required"
+          : !pRegex.test(e.target.value)
+          ? "wrong password"
+          : null,
+    });
+  };
+
+  let url = "#";
 
   return (
-      <>
-        <main className="container">
+    <>
+      <main className="container">
+        <img className="main__img" src={food} alt="..." />
 
-            <img className="main__img" src={food} alt="..." />
+        <section className="loginBox">
+          <h2 className="login__title">Login</h2>
+          <p className="login__text">
+            Log in with yout data that you entered during yout registration
+          </p>
 
-            <section className="loginBox">
-                <h2 className="login__title">Login</h2>
-                <p className="login__text">Log in with yout data that you entered during yout registration</p>
+          {/* Email */}
+          <form className="login__form" onSubmit={(e) => submitForm(e)}>
+            {/* Social Login {*/}
+            <div className="social-login">
+              <button
+                className="btn google-btn social-btn"
+                style={{ background: "red", marginBottom: "1.5rem" }}
+                type="button"
+                onClick={(e) => GoogleSignin(e)}
+              >
+                <span>
+                  <i className="fab fa-google-plus-g">Sign in with google+</i>
+                </span>
+              </button>
+              <button
+                className="btn facebook-btn social-btn"
+                style={{ background: "blue" }}
+                type="button"
+                onClick={(e) => FBSignin(e)}
+              >
+                <span>
+                  <i className="fab fa-facebook-f">Sign in with Facebook</i>
+                </span>
+              </button>
+            </div>
+            {/* } */}
+            <label for="email" className="emailLabel">
+              Your email <br />
+              <input
+                type="email"
+                className="email"
+                id="email"
+                placeholder="Example@example.com"
+                name="email"
+                value={login.email}
+                onChange={(e) => emailValidation(e)}
+              />
+              {errors.email && (
+                <small className="text-danger">{errors.email}</small>
+              )}
+            </label>
 
-                {/* Email */}
-                <form className="login__form" onSubmit={(e => submitForm(e))}>
-                    <label for="email" className="emailLabel">Your email <br />
-                        <input
-                        type="email"
-                        className="email"
-                        id="email"
-                        placeholder="Example@example.com"
-                        name="email"
-                        value={login.email}
-                        onChange={(e) => emailValidation(e)}
-                        />
-                        {errors.email && (<small className="text-danger">{errors.email}</small>)}
-                    </label>
+            {/* Password */}
+            <label for="password" className="passwordLabel">
+              Password <br />
+              <input
+                type="password"
+                className="password"
+                id="password"
+                placeholder="at least 8 characters"
+                name="password"
+                value={login.password}
+                onChange={(e) => passwordValidation(e)}
+              />
+              <small className="text-danger">{errors.password}</small>
+            </label>
 
-                    {/* Password */}
-                    <label for="password" className="passwordLabel">Password <br />
-                        <input
-                        type="password"
-                        className="password"
-                        id="password"
-                        placeholder="at least 8 characters"
-                        name="password"
-                        value={login.password}
-                        onChange={(e) => passwordValidation(e)}
-                        />
-                        <small className="text-danger">{errors.password}</small>
-                    </label>
+            <div className="btns">
+              <div>
+                <input type="checkbox" id="keep" className="keep" />
+                <label for="keep" className="keepLabel">
+                  Keep me logged in
+                </label>
+              </div>
+              <a className="forgotPassword" href={url}>
+                forgot Password?
+              </a>
+            </div>
+            <button className="login__btn">Log in</button>
+          </form>
 
-                    <div className="btns">
-                        <div>
-                            <input type="checkbox" id="keep" className="keep" />
-                            <label for="keep" className="keepLabel">Keep me logged in
-                        </label>
-                        </div>
-                        <a className="forgotPassword" href={url}>forgot Password?</a>
-                    </div>
-                    <button className="login__btn">Log in</button>
-                </form>
-
-                <div className="signup">
-                    <a href={url}>Don't have account?</a>
-                    <a href={url}>Sign Up</a>
-                </div>
-
-                
-            </section>
-        </main>
+          <div className="signup">
+            <a href={url}>Don't have account?</a>
+            <Link to="/register">Sign Up</Link>
+          </div>
+        </section>
+      </main>
     </>
   );
 }

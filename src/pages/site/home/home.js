2341c1bc4f72c3dc27cae285e0b46edd822c6e55
yousefomar724 <yousefomar724@gmail.com>
2021-11-11 06:33:58 +0200

    added recipeList, recipe components - responsive

--- src/pages/site/home/home.js
+++ src/pages/site/home/home.js
@@ -1,6 +1,7 @@
-import Nav from '../../../components/site/nav/nav'
+import RecipeList from '../../../components/site/recipesList/recipesList'
 import Search from '../../../components/site/search/search'
 import Hero from '../../../components/site/hero/hero'
+import Nav from '../../../components/site/nav/nav'
 import './home.css'
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       <Nav />
       <Search />
       <Hero />
+      <RecipeList />
     </main>
   )
 }

8e69888d8b5f7237e1d7c43d65f87ae210a1e46c
hebamsamy <hebasamy655@gmail.com>
2021-11-20 13:58:00 +0200

    debug select

--- src/components/admin/FormIngredients/ListComponent.js
+++ src/components/admin/FormIngredients/ListComponent.js
@@ -1,59 +1,60 @@
-
 import React from "react";
 import { useEffect, useState } from "react";
-import { collection, onSnapshot, addDoc,query,where,getDocs } from "firebase/firestore";
+import {
+  collection,
+  onSnapshot,
+  addDoc,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
 import { db } from "../../../firebase";
-
-
 
 const ListComponent = (props) => {
   const [Ingredients, setIngredients] = useState([]);
+  const [currentIngredient, setCurrentIngredient] = useState({});
   const [currentCat, setCurrentCat] = useState({});
   const [personNum, setPersonNum] = useState([]);
   const [Category_of_ingredients, setCatIngred] = useState([]);
   const [unit, setUnit] = useState("");
   const [quant, setQuant] = useState("");
 
+  useEffect(async () => {
+    await onSnapshot(
+      collection(db, "Categories_for_ingredients"),
+      (snapshot) => {
+        setCatIngred(
+          snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+        );
+      }
+    );
+    console.log("Category_of_ingredients", Category_of_ingredients);
+    setCurrentCat(Category_of_ingredients[0]);
+    console.log("Current Category", currentCat);
+    await TestQuery(currentCat.id);
+  }, []);
 
-
-
-  useEffect(
-    () =>{
-
-      onSnapshot(collection(db, "Categories_for_ingredients"), (snapshot) =>
-      
-      setCatIngred(snapshot.docs.map((doc) =>({...doc.data(),id:doc.id}) ))
-     
-      )
-    
-    
-    }
-    ,[]
-     
-  );
-
-  useEffect(
-    () =>
-    {
-     setCurrentCat(Category_of_ingredients[0]) 
-     console.log(currentCat);
-    }
-     
-      
-      ,
-    [Category_of_ingredients]
-  );
-  
-  async function TestQuery(id){
-    const q = query(collection(db,'Ingredients'),where("categoryId","==",`${id}`)); //!=
-    
+  const TestQuery = async (id) => {
+    console.log("call test query", id);
+    const q = query(
+      collection(db, "Ingredients"),
+      where("categoryId", "==", `${id}`)
+    );
     const querySnapshot = await getDocs(q);
-    console.log(querySnapshot);
-    querySnapshot.forEach((Ingredients)=>{
-        
-        console.log(Ingredients.data().ingName);
-    })
-}
+    let list = [];
+
+    querySnapshot.forEach((Ingredient) => {
+      let item = {
+        ...Ingredient.data(),
+        id: Ingredient.id,
+      };
+      console.log(item);
+      list.push(item)
+    });
+    console.log("list", list);
+    setIngredients(list);
+    console.log("set Ingredients", Ingredients);
+  };
 
   return (
     <div className="Component">
@@ -67,30 +68,24 @@ const ListComponent = (props) => {
           <select
             className="form-select form-control"
             id="Category_of_ingredients"
-            value={Category_of_ingredients}
+            value={currentCat}
             onChange={(e) => {
-              setCatIngred(e.target.value);
-              let value=e.target.value;
-              console.log(e.target.value);
-              TestQuery(value);
+              setCurrentCat(e.target.value);
+              TestQuery(e.target.value);
+              console.log("on change", e.target.value);
               //  let value = Array.from(e.target.selectedOptions, option => option.value);
               //  console.log(value);
               // setCategoryId(value);
             }}
           >
-            
-              
-                {Category_of_ingredients.map((Category_of_ingredient) => {
-                  return (
-                    <option value={Category_of_ingredient.id}>
-                      {" "}
-                      {Category_of_ingredient.ingCatName}{" "}
-                    </option>
-                  );
-                })}
-               
-            
-          
+            {Category_of_ingredients.map((Category_of_ingredient) => {
+              return (
+                <option value={Category_of_ingredient.id} selected>
+                  {" "}
+                  {Category_of_ingredient.ingCatName}{" "}
+                </option>
+              );
+            })}
           </select>
         </div>
         <div className="form-group text-right">
@@ -100,30 +95,30 @@ const ListComponent = (props) => {
           <select
             className="form-select form-control"
             id="recipeIngredient"
-            value={Ingredients}
-            onChange={(e) => setIngredients(e.target.value)}
-          
+            value={currentIngredient}
+            onChange={(e) => {
+              setCurrentIngredient(e.target.value);
+              console.log(e.target.value);
+            }}
           >
-            {Ingredients.map((Ingredient) =>  {
-              return (
-                <option value={Ingredient.id}>
-                {" "}
-                {Ingredient.ingName}{" "}
-              </option>
-              );
+            {Ingredients.map((Ingredient) => {
+              if (Ingredient)
+                return (
+                  <option value={Ingredient.id}> {Ingredient.ingName} </option>
+                );
             })}
-            
           </select>
         </div>
         <div className="form-group text-right">
-         
           <input
             type="number"
             className="form-control mt-2"
             id="quant"
             value={quant}
-            onChange={(e)=>{setQuant(e.target.value)
-              console.log(e.target.value)}}
+            onChange={(e) => {
+              setQuant(e.target.value);
+              console.log(e.target.value);
+            }}
             placeholder=" الكمية"
           />
           <input
@@ -132,23 +127,26 @@ const ListComponent = (props) => {
             id="personNum"
             placeholder=" تكفي كام شخص"
             value={personNum}
-            onChange={(e)=>{setPersonNum(e.target.value)
-              console.log(e.target.value)}}
+            onChange={(e) => {
+              setPersonNum(e.target.value);
+              console.log(e.target.value);
+            }}
           />
           <select
             className="form-select form-select-sm mt-2"
             aria-label=".form-select-sm example"
             value={unit}
-            onChange={(e)=>{setUnit(e.target.value)
-              console.log(e.target.value)}}
-            
+            onChange={(e) => {
+              setUnit(e.target.value);
+              console.log(e.target.value);
+            }}
           >
             <option selected>اختر الوحدة</option>
             <option value="جرام">جرام</option>
             <option value="كيلو">كيلو</option>
             <option value="لتر">لتر</option>
             <option value="كوب كبير"> كوب كبير</option>
-            <option value="كوب صغير">كوب  صغير</option>
+            <option value="كوب صغير">كوب صغير</option>
             <option value="معلقة كبيرة">معلقة كبيرة</option>
             <option value="معلقة صغيرة">معلقة صغيرة</option>
           </select>

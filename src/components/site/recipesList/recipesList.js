c5846eb754b3c163f4e415fc96d0d619144c58b9
AsmaaHosnyAly <49831110+AsmaaHosnyAly@users.noreply.github.com>
2021-11-27 21:04:59 +0200

    Dynmaic recipe

--- src/components/site/recipesList/recipesList.js
+++ src/components/site/recipesList/recipesList.js
@@ -1,15 +1,44 @@
 import './recipesList.css'
 import Recipe from '../recipe/recipe'
 import { data } from '../../../data/data'
+import { collection, getDocs,onSnapshot, } from '@firebase/firestore'
+import { db } from '../../../firebase'
+import { useState, useEffect } from 'react'
+
 
 const RecipeList = () => {
+  const [recipes, setRecipes] = useState([])
+
+  // --------------- Firebase
+  
+
+  
+
+
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "recipes"), (snapshot) =>
+      setRecipes(snapshot.docs.map((doc) =>({...doc.data(),id:doc.id}) ))
+      ),
+    []
+  );
+
+
+
   return (
-    <section className='recipe-list'>
-      {data.map((recipe, index) => {
-        return <Recipe {...recipe} key={index} />
-      })}
-    </section>
-  )
+    //  <section className='recipe-list'>
+    <>
+      {recipes.map((recipe) => {
+            return (
+             <div>
+             {recipe.recipeName}
+             </div>
+               
+            );
+          })}
+          </>
+  
+  );
 }
 
 export default RecipeList

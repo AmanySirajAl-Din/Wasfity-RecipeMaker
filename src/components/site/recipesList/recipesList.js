3fbf4b6aee8d8887d68cca872a8d3e043fa263ef
yousefomar724 <yousefomar724@gmail.com>
2021-11-27 20:12:43 +0200

    get firebase data

--- src/components/site/recipesList/recipesList.js
+++ src/components/site/recipesList/recipesList.js
@@ -1,12 +1,33 @@
 import './recipesList.css'
 import Recipe from '../recipe/recipe'
 import { data } from '../../../data/data'
+import { collection, getDocs } from '@firebase/firestore'
+import { db } from '../../../firebase'
+import { useState, useEffect } from 'react'
 
 const RecipeList = () => {
+  const [recipes, setRecipes] = useState([])
+
+  // --------------- Firebase
+  const colRef = collection(db, 'recipes')
+
+  useEffect(() => {
+    getDocs(colRef)
+      .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
+          setRecipes([...recipes, { ...doc.data(), id: doc.id }])
+          console.log(...doc.data())
+        })
+        console.log(recipes, 'onside')
+      })
+      .catch((e) => console.log(e.message))
+  }, [])
+  console.log(recipes)
+
   return (
     <section className='recipe-list'>
-      {data.map((recipe, index) => {
-        return <Recipe {...recipe} key={index} />
+      {recipes.map((recipe) => {
+        return <Recipe {...recipe} key={recipe.id} />
       })}
     </section>
   )
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 // I get it for make Social Login {
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
 //}
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -13,26 +13,26 @@ import "firebase/compat/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBqimLnK8l3nS4EKjjm2oE5jG0kvOQjhVA",
-  authDomain: "recipe-maker-system.firebaseapp.com",
-  projectId: "recipe-maker-system",
-  storageBucket: "recipe-maker-system.appspot.com",
-  messagingSenderId: "20370346274",
-  appId: "1:20370346274:web:1ab42a4d4d4cfe75cc957b",
-  measurementId: "G-P2SWYKJT8L",
-};
+  apiKey: 'AIzaSyBqimLnK8l3nS4EKjjm2oE5jG0kvOQjhVA',
+  authDomain: 'recipe-maker-system.firebaseapp.com',
+  projectId: 'recipe-maker-system',
+  storageBucket: 'recipe-maker-system.appspot.com',
+  messagingSenderId: '20370346274',
+  appId: '1:20370346274:web:1ab42a4d4d4cfe75cc957b',
+  measurementId: 'G-P2SWYKJT8L',
+}
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig)
 
-const db = getFirestore(app);
-const auth = getAuth(app);
+const db = getFirestore(app)
+const auth = getAuth(app)
 // I made it from improt (firebase/compat/app) and (firebase/compat/auth) to make social login{
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
-const auth2 = firebase.auth();
+const auth2 = firebase.auth()
 
-const google = new firebase.auth.GoogleAuthProvider();
+const google = new firebase.auth.GoogleAuthProvider()
 
-const facebook = new firebase.auth.FacebookAuthProvider();
-export { db, auth, auth2, google, facebook };
+const facebook = new firebase.auth.FacebookAuthProvider()
+export { db, auth, auth2, google, facebook }
